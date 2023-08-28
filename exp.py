902d761e63f4bc4c0eb8f04890fc761562b04b4a
ziedammak <41289114+ziedammak@users.noreply.github.com>
2023-08-28 12:16:26 +0100

    Add files via upload

---
+++ exp.py
@@ -0,0 +1,10 @@
+from chat_library import Chat
+
+api_key = "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"
+system_prompt = "You are a helpful assistant.\n"
+chatbot = Chat(api_key, system_prompt)
+
+response = chatbot.ask("Hello, how can you help?")
+print(response)
+
+chatbot.reset()
