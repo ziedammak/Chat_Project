b023c72ce084c59e754cd6d737f709f1cd72a5a7
ziedammak <41289114+ziedammak@users.noreply.github.com>
2023-08-30 17:57:52 +0100

    Update chat_with_ai.py

--- chat_library/chat_with_ai/chat_with_ai.py
+++ chat_library/chat_with_ai/chat_with_ai.py
@@ -1,20 +1,22 @@
 import openai
 
 class Chat:
-    def __init__(self, api_key, system_prompt):
+    def __init__(self, api_key, system_prompt, model="gpt-3.5-turbo"):
         openai.api_key = api_key
         self.system_prompt = system_prompt
         self.chat_history = ""
+        self.model = model
 
     def ask(self, user_input):
         prompt = f"{self.system_prompt}\nUser: {user_input}\nAI:"
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=100,
+        response = openai.ChatCompletion.create(  # Use ChatCompletion for chat models
+            model=self.model,
+            messages=[
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": user_input},
+            ],
         )
-        message = response.choices[0].text.strip()
+        message = response.choices[0].message["content"]
         self.chat_history += f"\nUser: {user_input}\nAI: {message}"
         return message
 
