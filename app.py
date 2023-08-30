af836741886e932a2ef006e0e739a26fa125802e
ziedammak <41289114+ziedammak@users.noreply.github.com>
2023-08-30 16:56:12 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+from chat_library.chat_with_ai.chat_with_ai  import Chat
+app = Flask(__name__,template_folder='web_interface/templates')
+
+api_key = "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"
+system_prompt = "You are a helpful assistant.\n"
+chatbot = Chat(api_key, system_prompt)
+
+chat_history = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        chat_history.append({"role": "user", "content": user_input})
+        ai_response = chatbot.ask(user_input)
+        chat_history.append({"role": "ai", "content": ai_response})
+    return render_template("chat.html", chat_history=chat_history)
+
+@app.route("/get_response", methods=["POST"])
+def get_response():
+    user_input = request.json.get("user_input")
+    chat_history.append({"role": "user", "content": user_input})
+    ai_response = chatbot.ask(user_input)
+    chat_history.append({"role": "ai", "content": ai_response})
+    return jsonify({"ai_response": ai_response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
