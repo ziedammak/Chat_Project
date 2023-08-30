12a91dd31d38a93b83869a9d8ca3e41bc1227b72
ziedammak <41289114+ziedammak@users.noreply.github.com>
2023-08-30 16:58:24 +0100

    Update setup.py

--- chat_library/setup.py
+++ chat_library/setup.py
@@ -1,1 +1,13 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name='chat_with_ai',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+    ],
+    license='MIT',
+    url='https://github.com/ziedammak/Chat_Project',
+)
